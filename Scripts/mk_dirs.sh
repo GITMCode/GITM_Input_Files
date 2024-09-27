dcbe92a1fda311021d8cdc9feae952e087bc86c1
Aaron Ridley <ridley@umich.edu>
2024-09-27 13:24:50 -0400

    FEAT: example script for making a bunch of directories

---
+++ Scripts/mk_dirs.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+YEAR=2012
+MONTH=03
+DAY=07
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=04
+DAY=23
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=06
+DAY=16
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=07
+DAY=15
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=09
+DAY=30
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=10
+DAY=07
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=11
+DAY=13
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+YEAR=2013
+MONTH=05
+DAY=31
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+
+MONTH=06
+DAY=28
+mkdir ${YEAR}/${MONTH}
+mkdir ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
+cp Scripts/mkrun.sh ${YEAR}/${MONTH}/${YEAR}${MONTH}${DAY}.Storm
