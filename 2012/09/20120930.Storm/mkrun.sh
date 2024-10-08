698b54b481a9033f66db9754acc36a0ae1db6ff4
Aaron Ridley <ridley@umich.edu>
2024-10-08 07:12:25 -0400

    change to 15 min output for start run

--- 2012/09/20120930.Storm/mkrun.sh
+++ 2012/09/20120930.Storm/mkrun.sh
@@ -17,10 +17,10 @@ let CPUS=${LATS}*${LONS}
 WALLTIME=72
 
 # This is for the post processor on pleiades:
-echo "ridley\nmia\n/nobackup/Gitm/Runs/${START}.Storm/v1" > remote
+echo "ridley\nzed\n/nobackup/Gitm/Runs/${START}.Storm/v1" > remote
 
 # Make two UAM.in files (start and restart):
-${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=find -fism -dynamo -fang -fta -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
+${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=find -fism -dynamo -fang -fta -nlats=${LATS} -nlons=${LONS} -3dall=900 -2danc=900 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
 
 # Make another UAM.in file for running with Newell:
 ${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in.newell -imf=imf${START}.dat -sme=ae${START}.dat -fism -dynamo -fang -newell -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} ${START} ${END}
