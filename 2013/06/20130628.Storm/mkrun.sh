7d16421ac1d7e3f9db8272f6ef98a10581fae238
Aaron Ridley <ridley@umich.edu>
2025-09-12 16:04:27 -0400

    update to develop branch

--- 2013/06/20130628.Storm/mkrun.sh
+++ 2013/06/20130628.Storm/mkrun.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
-# where to find gitm:
-GITM=/Users/ridley/Software/GITM
-
 # set the dates with a restart in the middle:
 START=20130626
 RESTART=20130628
 END=20130701
 
+# ------------------------------------------------------------------
+
+# where to find gitm:
+GITM=~/Software/GITM
+
 # set number of lat and lon blocks:
-LATS=40
-LONS=20
-let CPUS=${LATS}*${LONS}
+LATS=20
+LONS=10
+CPUS=$((${LATS}*${LONS}))
 
 # time in hours for wall time:
-WALLTIME=72
+WALLTIME=24
 
 # This is for the post processor on pleiades:
-echo "ridley\nmia\n/nobackup/Gitm/Runs/${START}.Storm/v1" > remote
+echo "ridley\nmia\n/nobackup/Gitm/Runs/${START}.Storm/vMSIS_60" > remote
 
 # Make two UAM.in files (start and restart):
-${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=find -fism -dynamo -fang -fta -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
-
-# Make another UAM.in file for running with Newell:
-${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in.newell -imf=imf${START}.dat -sme=ae${START}.dat -fism -dynamo -fang -newell -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} ${START} ${END}
+${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=find -fism -dynamo -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
 
 # make two job files (start and restart):
 # rule of thumb would be 2 days of sim / 24 hours
