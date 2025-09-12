bad8bc767840dea69b6f90e57e7585c11157eed7
Aaron Ridley <ridley@umich.edu>
2025-09-12 16:00:34 -0400

    update to develop branch

---
+++ 2013/03/20130317.Storm/mkrun.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# set the dates with a restart in the middle:
+START=20130316
+RESTART=20130317.0600
+END=20130318.0600
+
+# ------------------------------------------------------------------
+
+# where to find gitm:
+GITM=~/Software/GITM
+
+# set number of lat and lon blocks:
+LATS=20
+LONS=10
+CPUS=$((${LATS}*${LONS}))
+
+# time in hours for wall time:
+WALLTIME=24
+
+# This is for the post processor on pleiades:
+echo "ridley\nmia\n/nobackup/Gitm/Runs/${START}.Storm/vMSIS_60" > remote
+
+# Make two UAM.in files (start and restart):
+${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=find -fism -dynamo -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
+
+# make two job files (start and restart):
+# rule of thumb would be 2 days of sim / 24 hours
+${GITM}/srcPython/gitm_makejob.py -gitm=${GITM} -output=job -name=g${START} -wall=${WALLTIME} -gid=s2516 -cpus=${CPUS}
+${GITM}/srcPython/gitm_makejob.py -gitm=${GITM} -output=job.r -name=g${START}r -wall=${WALLTIME} -gid=s2516 -cpus=${CPUS} -restart
+
