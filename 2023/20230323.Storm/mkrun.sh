3ca7828a2136ed58a8c0751fdd087a38734c3677
Aaron Ridley <ridley@umich.edu>
2026-07-23 17:26:58 -0400

    March 23, 2023 Storm

---
+++ 2023/20230323.Storm/mkrun.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# set the dates with a restart in the middle:
+START=20230321
+RESTART=20230323.0000
+END=20230326
+
+PID=s2516
+SUBDIR=vMSIS_60
+
+# where to find gitm:
+GITM=~/Software/GITM
+
+# set number of lat and lon blocks:
+LATS=20
+LONS=10
+CPUS=$((${LATS}*${LONS}))
+
+# time in hours for wall time:
+WALLTIME=48
+
+# This is for the post processor on pleiades:
+echo "ridley\nmia\n/nobackup/Gitm/Runs/${START}.Storm/${SUBDIR}" > remote
+
+# Make two UAM.in files (start and restart):
+${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in -imf=find -sme=sme20230323.dat -fism -dynamo -fang -fta -nlats=${LATS} -nlons=${LONS} -3dall=900 -2danc=900 -restart -cputimemax=${WALLTIME} -restarttime=${RESTART} ${START} ${END}
+
+# Make another UAM.in file for running with Newell:
+#${GITM}/srcPython/gitm_makerun.py -gitm=${GITM} -output=UAM.in.newell -imf=imf${START}.dat -sme=ae${START}.dat -fism -dynamo -fang -newell -nlats=${LATS} -nlons=${LONS} -3dall=300 -2danc=300 -restart -cputimemax=${WALLTIME} ${START} ${END}
+
+# make two job files (start and restart):
+# rule of thumb would be 2 days of sim / 24 hours
+${GITM}/srcPython/gitm_makejob.py -gitm=${GITM} -output=job -name=g${START} -wall=${WALLTIME} -gid=${PID} -cpus=${CPUS}
+${GITM}/srcPython/gitm_makejob.py -gitm=${GITM} -output=job.r -name=g${START}r -wall=${WALLTIME} -gid=${PID} -cpus=${CPUS} -restart
+
